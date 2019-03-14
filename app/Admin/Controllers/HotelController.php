535304ee6055ea8dfd21ad17061e9fe142659b5d
tamvo <tamvobiz@gmail.com>
2019-03-15 03:30:25 +0700

    Add Admin HotelController [buggy]

--- app/Admin/Controllers/HotelController.php
+++ app/Admin/Controllers/HotelController.php
@@ -3,12 +3,13 @@
 namespace Honviettour\Admin\Controllers;
 
 use Honviettour\Models\Hotel;
-use App\Http\Controllers\Controller;
+use Honviettour\Http\Controllers\Controller;
 use Encore\Admin\Controllers\HasResourceActions;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
+use Honviettour\Models\Country;
 
 class HotelController extends Controller
 {
@@ -80,16 +81,20 @@ class HotelController extends Controller
     protected function grid()
     {
         $grid = new Grid(new Hotel);
+        $grid->paginate(config('constants.ADMIN_ITEM_PER_PAGE'));
+        $grid->disableRowSelector();
 
         $grid->id('Id');
+        $grid->images('Photo')->display(function () {
+            return count($this->images) ? '<img width="30" src="'  .(env('APP_URL') . '/storage/' . $this->images[0]['path']) . '""/>' : '';
+        });
+
         $grid->name('Name');
-        $grid->description('Description');
+        $grid->description('Description')->limit(20);
         $grid->address('Address');
-        $grid->country('Country');
-        $grid->photo('Photo');
-        $grid->service('Service');
-        $grid->latitude('Latitude');
-        $grid->longitude('Longitude');
+        $grid->column('Country')->display(function () {
+            return $this->countryInfo['name'];
+        });
         $grid->created_at('Created at');
         $grid->updated_at('Updated at');
 
@@ -131,13 +136,40 @@ class HotelController extends Controller
         $form = new Form(new Hotel);
 
         $form->text('name', 'Name');
-        $form->textarea('description', 'Description');
+        $form->hasMany('images', 'Photo', function(Form\NestedForm $form) {
+            $form->image('path', 'Photo');
+            $form->switch('status', 'Published');
+            $form->hidden('model_type');
+        });
         $form->text('address', 'Address');
-        $form->text('country', 'Country');
-        $form->text('photo', 'Photo');
-        $form->text('service', 'Service');
+        $form->select('country', 'Country')->options(Country::all()->pluck('name', 'id'));
+        $form->textarea('description', 'Description')->rows(4);
         $form->decimal('latitude', 'Latitude');
         $form->decimal('longitude', 'Longitude');
+        $form->summernote('service', 'Service');
+
+        $form->switch('status', 'Published');
+        $form->display('created_at', 'Created At');
+        $form->display('updated_at', 'Updated At');
+
+         $form->saving(function (Form $form) {
+            $form->images = array_map(function($item) {
+                $item['model_type'] = get_class(new Hotel);
+                return $item;
+            }, $form->images);
+        });
+
+        $form->footer(function ($footer) {
+            // disable `View` checkbox
+            $footer->disableViewCheck();
+
+            // disable `Continue editing` checkbox
+            $footer->disableEditingCheck();
+
+            // disable `Continue Creating` checkbox
+            $footer->disableCreatingCheck();
+
+        });
 
         return $form;
     }
