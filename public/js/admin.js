612a48191ee95c36a80b4764055f4a38d7f2799c
tamvo <tamvobiz@gmail.com>
2019-05-04 02:12:26 +0700

    fix page js function

--- public/js/admin.js
+++ public/js/admin.js
@@ -3,20 +3,16 @@ $(function() {
 })
 
 function checkCategory() {
-    var currentPage = window.location.pathname;
-    if(currentPage.includes('news')) {
-        $('#code').closest('.form-group').hide();
-        $('#expire_date').closest('.form-group').hide();
-        $('.category').on('change', function() {
-            if($(this).val() == 1) {
-                $('#code').closest('.form-group').show();
-                $('#expire_date').closest('.form-group').show();
-            } else {
-                $('#code').closest('.form-group').hide();
-                $('#expire_date').closest('.form-group').hide();
-            }
-        })
-
-    }
+    $('#code').closest('.form-group').hide();
+    $('#expire_date').closest('.form-group').hide();
+    $('.category').on('change', function() {
+        if($(this).val() == 1) {
+            $('#code').closest('.form-group').show();
+            $('#expire_date').closest('.form-group').show();
+        } else {
+            $('#code').closest('.form-group').hide();
+            $('#expire_date').closest('.form-group').hide();
+        }
+    })
 }
 
