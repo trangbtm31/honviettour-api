1ff7d56645cb2478be45f651cdc17d6a59da0d73
Trang <trangbtm31@gmail.com>
2019-10-13 18:11:51 +0700

    Update type for storing start date of schedule

---
+++ database/migrations/2019_10_13_160246_update_schedulel.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateSchedulel extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('schedules', function (Blueprint $table) {
+            $table->string('start_date')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('schedule', function (Blueprint $table) {
+            //
+        });
+    }
+}
